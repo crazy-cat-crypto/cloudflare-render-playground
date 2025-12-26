a48feb00f9a7842841c56732c36dd167036ca3bd
crazy-cat-crypto <226846482+crazy-cat-crypto@users.noreply.github.com>
2025-12-26 11:37:23 +0545

    Refactor Flask app to include CORS and update API endpoint to return JSON response

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask,jsonify
+from flask_cors import CORS
 
 app=Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def home():
-    return "Hello World!"
+@app.route("/api/hello")
+def hello():
+    return jsonify({"message":"Namaste from flask"})
 
-
-if __name__=="__main__":
-    app.run(debug=True)
